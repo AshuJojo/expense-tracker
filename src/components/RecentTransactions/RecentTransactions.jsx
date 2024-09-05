dbc0cb3c3c923a370fe484c0eec4fa798be58004
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-09-05 13:54:15 +0530

    Pagination Implemeneted in Recent Transaction Component UI

--- src/components/RecentTransactions/RecentTransactions.jsx
+++ src/components/RecentTransactions/RecentTransactions.jsx
@@ -2,33 +2,69 @@ import { TiDeleteOutline } from 'react-icons/ti';
 import styles from './RecentTransactions.module.css';
 import { PiGiftLight, PiPizzaLight, PiSuitcaseRollingLight } from 'react-icons/pi';
 import { FiEdit2 } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
+import { GoArrowLeft, GoArrowRight } from 'react-icons/go';
 
 function RecentTransactions() {
     const data = [
         {
+            id: 1,
             title: 'Samosa',
             price: 150,
             category: 'food',
             transactionDate: 'March 20, 2024'
         },
         {
+            id: 2,
             title: 'Movie',
             price: 300,
             category: 'entertainment',
             transactionDate: 'March 21, 2024'
         },
         {
+            id: 3,
             title: 'Auto',
             price: 50,
             category: 'travel',
             transactionDate: 'March 22, 2024'
         },
+        {
+            id: 4,
+            title: 'Auto',
+            price: 510,
+            category: 'travel',
+            transactionDate: 'March 22, 2024'
+        },
     ]
+
+    const [page, setPage] = useState(1);
+    const [filteredData, setFilteredData] = useState([]);
+
+    const handlePagePrev = () => {
+        if (page !== 1) {
+            setPage(page - 1);
+        }
+    }
+
+    const handlePageNext = () => {
+        if ((page) * 3 + 1 <= data.length) {
+            setPage(page + 1);
+        }
+    }
+
+    useEffect(() => {
+        const end = 3 * page;
+        const start = end - 3;
+
+        const tempData = data.slice(start, end);
+        setFilteredData(tempData);
+    }, [page]);
+
 
     return (
         <div className={styles.Container}>
-            {data.map((item) => {
-                return <>
+            {filteredData.map((item) => {
+                return <div className={styles.TransactionItemContainer} key={item.id}>
                     <div className={styles.Transaction}>
                         <div className={styles.Info}>
                             <div className={styles.Detail}>
@@ -51,9 +87,18 @@ function RecentTransactions() {
                             <button type='button' className={`${styles.IconBtn} ${styles.EditBtn}`}><FiEdit2 /></button>
                         </div>
                     </div>
-                    <hr /></>
+                    <hr className={styles.Divider} />
+                </div>
             })}
-        </div>
+
+            <div className={styles.Pagination}>
+                <button type='button' className={styles.PaginationBtn} onClick={handlePagePrev}><GoArrowLeft /></button>
+                <div className={styles.PageNumber}>
+                    {page}
+                </div>
+                <button type='button' className={styles.PaginationBtn} onClick={handlePageNext}><GoArrowRight /></button>
+            </div>
+        </div >
     )
 }
 
